8b6ac918d11dde32669e0d0fe8e6792fd1966e5d
Aji Maulana <mancode700@gmail.com>
2024-11-23 13:50:39 +0000

    update

--- api/index.js
+++ api/index.js
@@ -3,8 +3,6 @@ const http = require("http");
 const app = require("../src/app");
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
