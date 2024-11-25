3cdbc5923e407c2748c45829c1a6f3acc6fc3e92
Aji Maulana <mancode700@gmail.com>
2024-11-25 09:28:07 +0000

    update db

--- src/db.js
+++ src/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(DB_URI);
     console.log('✔ MongoDB connected successfully');
-
-    isConnected = true;  
   } catch (error) {
     console.error('✘ MongoDB connection error:', error.message);
     process.exit(1); // Berhenti jika gagal koneksi
