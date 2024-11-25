fa38fe38b0b6d14c1a234d5d9daa0bfaa6197b16
Aji Maulana <mancode700@gmail.com>
2024-11-25 09:33:46 +0000

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import http from "http";
 import app from "./app.js";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server berjalan di port 3000");
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
