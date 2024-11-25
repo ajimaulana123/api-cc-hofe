72d3a2fc1ee192e53162053535ed04a3e85f38d7
Aji Maulana <mancode700@gmail.com>
2024-11-25 09:38:11 +0000

    uji coba

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import http from "http";
 import app from "./app.js";
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+import { disconnectDB } from './db.js';
 
-const server = http.createServer(app);
+// Fungsi untuk menangani graceful shutdown
+const startServer = async () => {
+  const server = http.createServer(app).listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });  
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  // Graceful shutdown untuk menangani SIGINT (Ctrl+C) dan SIGTERM
+  process.on('SIGINT', async () => {
+    console.log('Menerima SIGINT, menutup aplikasi...');
+    await disconnectDB(); // Menutup koneksi ke database
+    server.close(() => {
+      console.log('Server berhasil dimatikan');
+      process.exit(0); // Keluar dari aplikasi
+    });
+  });
+
+  process.on('SIGTERM', async () => {
+    console.log('Menerima SIGTERM, menutup aplikasi...');
+    await disconnectDB(); // Menutup koneksi ke database
+    server.close(() => {
+      console.log('Server berhasil dimatikan');
+      process.exit(0); // Keluar dari aplikasi
+    });
+  });
+};
+
+startServer(); // Mulai server
